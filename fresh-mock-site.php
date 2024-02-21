2eaca246711510295c7d37cff76fc05946aa232a
YauheniNikifarau <Yauheni.Nikifarau@itechart-group.com>
2024-02-21 14:41:18 +0300

    csv and plans

---
+++ fresh-mock-site.php
@@ -0,0 +1,13 @@
+<?php
+
+// Remove all
+// Remove all campaigns
+// Run BM bulker
+// Create Section Why Trust
+// Create Author Group
+// Create Best Bookies
+// Create Ranking Section
+// Create Campaign
+// Create Free Bets
+// Create Homepage
+// Create Page
